6c45d7aeb36c4f15367c042b77bdc2b2e1d17e40
Austin <austinlongocontracting@gmail.com>
2021-12-01 21:12:20 -0500

    commit tree beaf466d18a0f190b1542eb0f06d3bec91ee8c3c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(toDo='roller-skate') {
+    return `This Saturday, I want to ${toDo}!`;
+}
+
+function mondayWork(toDo='go to the office') {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(prompt = '*'){
+    
+    let inner = function (adj = 'special') {
+        return `You are ${prompt}${adj}${prompt}!`;
+    };
+
+    return inner;
+}
+
+console.log(wrapAdjective('dog', 'dogs'));
